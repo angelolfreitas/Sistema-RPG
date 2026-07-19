3e735b9ad3aaee14a82be21e1fb779ea67a5f78d
angelolfreitas <angeloantonio202020@gmail.com>
2026-07-19 13:10:27 -0300

    fazer dovimentacao dos pacontes infra depois. A fazer documentacao dos DTOS

--- src/main/java/com/ieji/rpg/infra/security/SecurityFilter.java
+++ src/main/java/com/ieji/rpg/infra/security/SecurityFilter.java
@@ -19,7 +19,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
-//estudar filter depois
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
